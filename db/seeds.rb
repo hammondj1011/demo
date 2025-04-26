0dfe1badfada3739a19e394806d31d47f58d52c0
Jeff Hammond <hammondj1011@gmail.com>
2025-04-25 20:06:34 -0500

    testing git

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Book.destroy_all
+
+Book.create!([{
+"title":"Inside Out",
+"author":"Pete Doctor",
+"genre":"Comedy",
+"description":"This was a silly book",
+"published_at":2025-01-01},
+{
+"title":"The Awakening",
+"author":"Joe Smith",
+"genre":"Sci-Fi",
+"description":"This was a sci-fi book",
+"published_at":2025-04-01},
+{
+"title":"Flowers for Sally",
+"author":"Ellie Adams",
+"genre":"Romance",
+"description":"This was a romantic book",
+"published_at":2020-04-01}])
+
+p "Created #{Book.count} movies"
